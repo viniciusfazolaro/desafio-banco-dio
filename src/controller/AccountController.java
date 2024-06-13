5ae1f347d906e34d5539e9ac6d5fa42e7003dd1d
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2024-06-12 22:26:18 -0300

    fix: printStatement error

--- src/controller/AccountController.java
+++ src/controller/AccountController.java
@@ -17,7 +17,6 @@ public class AccountController{
     
     public AccountController() { }
 
-    
     public void addAccount(String accType) {
         Scanner sc = new Scanner(System.in);
     
@@ -37,10 +36,17 @@ public class AccountController{
                 if (!accountExists) {
                     if (accType.equals("2")) {
                         CheckingAccount newAccount = new CheckingAccount(c);
-                        if (cAccounts.add(newAccount) && accounts.add(newAccount)) System.out.println("Conta corrente cadastrada com sucesso!"); 
+                        if (cAccounts.add(newAccount) && accounts.add(newAccount)){
+                            System.out.println("Conta corrente cadastrada com sucesso!");
+                            newAccount.printInfo();
+                        }
                     } else {
                         SavingsAccount newAccount = new SavingsAccount(c);
-                        if (sAccounts.add(newAccount) && accounts.add(newAccount)) System.out.println("Conta poupança cadastrada com sucesso!");
+                        if (sAccounts.add(newAccount) && accounts.add(newAccount)){ 
+                            System.out.println("Conta poupança cadastrada com sucesso!");
+                            newAccount.printInfo();
+                        }
+
                     }
                 } else System.out.println("Cliente já possui conta do tipo selecionado!");
             }
@@ -50,7 +56,7 @@ public class AccountController{
         sc.nextLine();
     }
 
-    public boolean hasAccounts(String accType, String cpf) {
+    private boolean hasAccounts(String accType, String cpf) {
         if (accType.equals("2")) {
             for(CheckingAccount c : cAccounts) if(c.getClient().getCpf().equals(cpf)) return true; 
         } else {
@@ -60,7 +66,7 @@ public class AccountController{
         return false;
     }
 
-    public boolean findAccbyNumber(int number) { return accounts.stream().anyMatch(a -> a.getNumber() == number); }
+    private boolean findAccbyNumber(int number) { return accounts.stream().anyMatch(a -> a.getNumber() == number); }
 
     public void listAccounts() {
         Scanner sc = new Scanner(System.in);
@@ -70,6 +76,7 @@ public class AccountController{
             System.out.println("Digite o tipo de conta (1 - Corrente | 2 - Poupança | 3 - Ambas):");
             option = sc.nextLine();
         } while (!option.equals("1") && !option.equals("2") && !option.equals("3"));
+        
         if (option.equals("1")) listCheckingAccounts();
         else if (option.equals("2")) listSavingsAccounts();
         else {
@@ -124,7 +131,6 @@ public class AccountController{
         for(Account a : accounts) {
             if(a.getNumber() == number) {
                 a.deposit(amount);
-                System.out.println("Depósito realizado com sucesso!");
                 break;
             }
         }
@@ -155,24 +161,76 @@ public class AccountController{
         do {
             System.out.println("Digite o valor do saque:");
             double amount = sc.nextDouble();
-                for(CheckingAccount c : cAccounts) {
-                    if(accFound = findAccbyNumber(number)) {
-                        c.withdraw(amount);
-                        break;
-                    }
+            for(CheckingAccount c : cAccounts) {
+                if(accFound = findAccbyNumber(number)) {
+                    c.withdraw(amount);
+                    break;
                 }
-                for(SavingsAccount s : sAccounts) {
-                    if(accFound = findAccbyNumber(number)) {
-                        s.withdraw(amount);
-                        break;
-                    }
+            }
+            for(SavingsAccount s : sAccounts) {
+                if(accFound = findAccbyNumber(number)) {
+                    s.withdraw(amount);
+                    break;
                 }
+            }
 
             if (!accFound) System.out.println("Conta não encontrada!");
 
         } while (!accFound);
 
         System.out.println("Pressione ENTER para continuar...");
+        sc.nextLine();
+        sc.nextLine();
+    }
+
+    public void transfer() {
+        Scanner sc = new Scanner(System.in);
+
+        int originAccount = 0;
+        int destinationAccount = 0;
+
+        boolean originAccFound = false;
+        boolean destinationAccFound = false;
+
+        do {
+
+            System.out.println("Digite o número da conta de origem:");
+            originAccount = sc.nextInt();
+
+            originAccFound = findAccbyNumber(originAccount);
+
+            if (!originAccFound) System.out.println("Conta de origem não encontrada!");
+
+        } while (!originAccFound);
+
+        do {
+
+            System.out.println("Digite o número da conta de destino:");
+            destinationAccount = sc.nextInt();
+
+            destinationAccFound = findAccbyNumber(destinationAccount);
+
+            if (!destinationAccFound) System.out.println("Conta de destino não encontrada!");
+            if (originAccount == destinationAccount) System.out.println("Conta de origem e destino não podem ser iguais!");
+
+        } while (!destinationAccFound || originAccount == destinationAccount);
+
+        System.out.println("Digite o valor da transferência:");
+        double amount = sc.nextDouble();
+
+        for (Account a : accounts) {
+            if (a.getNumber() == originAccount) {
+                for (Account b : accounts) {
+                    if (b.getNumber() == destinationAccount && a != b) {
+                        a.transfer(amount, b);
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Pressione ENTER para continuar...");
+
         sc.nextLine();
         sc.nextLine();
     }
@@ -196,7 +254,7 @@ public class AccountController{
         accFound = false;
 
         for(Account a : accounts) {
-            if(findAccbyNumber(number)) {
+            if(a.getNumber() == number){
                 a.printStatement();
                 accFound = true;
                 break;

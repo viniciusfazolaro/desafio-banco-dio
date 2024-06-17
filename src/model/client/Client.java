df24755a8aab2aa24573365ea261b16d4f3512f1
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2024-06-16 23:23:04 -0300

    refactor: add comments

--- src/model/client/Client.java
+++ src/model/client/Client.java
@@ -5,11 +5,13 @@ public class Client {
     @Getter private String name;
     @Getter private String cpf;
     
+    // Construtor
     public Client(String name, String cpf) {
         this.name = name;
         this.cpf = cpf;
     }
 
+    // método para imprimir dados do cliente
     public void printInfo() {
         System.out.println("*** Dados do Cliente ***");
         System.out.println(String.format("Nome: %s", name));

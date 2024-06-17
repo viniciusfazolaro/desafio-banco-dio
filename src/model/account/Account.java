f0d9f92d2e797bbf605d895d5e8da72257e4101c
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2024-06-16 23:21:25 -0300

    refactor: remove unnecessary getters and add some comments

--- src/model/account/Account.java
+++ src/model/account/Account.java
@@ -7,15 +7,18 @@ import java.util.List;
 
 public abstract class Account implements IAccount {
 
+    // Atributos estáticos
     private static final int DEFAULT_AGENCY = 0001;
     private static int ID = 1;
 
-    @Getter protected int agency;
+    // Atributos
+    protected int agency;
+    protected double balance;
     @Getter protected int number;
-    @Getter protected double balance;
     @Getter protected Client client;
-    @Getter protected List<String> transactions = new ArrayList<>();
+    protected List<String> transactions = new ArrayList<>(); // lista de transações
 
+    // Construtor
     public Account(Client client) {
         this.agency = DEFAULT_AGENCY;
         this.number = ID++;
@@ -23,41 +26,43 @@ public abstract class Account implements IAccount {
     }
 
     @Override
-    public void withdraw(double amount) {
-        if (amount > balance) {
+    public void withdraw(double amount) { // método para sacar
+        if (amount > balance) { // verifica se o valor do saque é maior que o saldo
             System.out.println("Saldo insuficiente!");
-        } else {
+        } else { // realiza o saque
             balance -= amount;
-            transactions.add(String.format("Saque de R$%.2f", amount));
-            System.out.println("Saque efetuado com sucesso!");
+            transactions.add(String.format("Saque de R$%.2f", amount)); // adiciona a transação na lista
+            System.out.println(String.format("Saque de R$%.2f efetuado com sucesso!", amount));
         }
     }
 
     @Override
-    public void deposit(double amount) {
-        balance += amount;
-        transactions.add(String.format("Depósito de R$%.2f", amount));
-        System.out.println("Depósito efetuado com sucesso!");
+    public void deposit(double amount) { // método para depositar
+        balance += amount; // adiciona o valor ao saldo
+        transactions.add(String.format("Depósito de R$%.2f", amount)); // adiciona a transação na lista
+        System.out.println(String.format("Depósito de R$%.2f efetuado com sucesso", amount));
     }
 
     @Override
-    public void transfer(double amount, Account destinationAccount) {
-        if (amount > balance) System.out.println("Saldo insuficiente para transferência!");
-        else {
-            this.balance -= amount;
-            destinationAccount.balance += amount;
-            this.transactions.add(String.format("Transferência de R$%.2f para %s", amount, destinationAccount.getClient().getName()));
-            destinationAccount.transactions.add(String.format("Transferência de R$%.2f recebida de %s", amount, this.client.getName()));
+    public void transfer(double amount, Account destinationAccount) { // método para transferir
+        if (amount > balance) System.out.println("Saldo insuficiente para transferência!"); // verifica se o valor da transferência é maior que o saldo
+        else { // realiza a transferência
+            this.balance -= amount; // subtrai o valor do saldo
+            destinationAccount.balance += amount; // adiciona o valor ao saldo da conta de destino
+            this.transactions.add(String.format("Transferência de R$%.2f para %s", amount, destinationAccount.getClient().getName())); // adiciona a transação na lista
+            destinationAccount.transactions.add(String.format("Transferência de R$%.2f recebida de %s", amount, this.client.getName())); // adiciona a transação na lista da conta de destino
             System.out.println("Transferência realizada com sucesso!");
         }
     }
 
-    public void printStatement() {
+    @Override
+    public void printStatement() { // método para imprimir extrato
         System.out.println("*** Extrato da Conta ***");
         for (String transaction : transactions) System.out.println(transaction);
         System.out.println("***********************");
     }
 
+    // método para imprimir informações da conta
     protected void printCommonInfo() {
         System.out.println("*** Dados da Conta ***");
         System.out.println(String.format("Titular: %s", client.getName()));

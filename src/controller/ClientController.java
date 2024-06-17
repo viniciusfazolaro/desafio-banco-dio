fea34c83604dfd280b15b9a07284e69bedf617ab
viniciusfazolaro <vinicius.fazolaro@gmail.com>
2024-06-16 23:20:50 -0300

    refactor: add some comments and remove unnecessary code

--- src/controller/ClientController.java
+++ src/controller/ClientController.java
@@ -8,11 +8,12 @@ import model.client.Client;
 
 public class ClientController {
     
-    private static List<Client> clients = new ArrayList<>();
+    private static List<Client> clients = new ArrayList<>(); // Lista de clientes
     
-    public ClientController() {
-    }
+    // Construtor
+    public ClientController() { } 
 
+    // metodo para adicionar cliente
     public void addClient() {
         Scanner sc = new Scanner(System.in);
 
@@ -20,14 +21,14 @@ public class ClientController {
         String cpf = null;
         Client cCpf = null;
 
-        do {
+        do { // enquanto o nome for menor que 3 caracteres
             System.out.println("Digite o nome do cliente:");
             name = sc.nextLine();
 
-            if(name.length() < 3 && name.contains("")) System.out.println("Nome deve conter no mínimo 3 caracteres!");
+            if(name.length() < 3) System.out.println("Nome deve conter no mínimo 3 caracteres!"); 
         } while(name.length() < 3);
 
-        do {
+        do { // enquanto o cpf for menor que 11 caracteres ou já existir
             System.out.println("Digite o CPF do cliente:");
             cpf = sc.nextLine();
 
@@ -36,7 +37,7 @@ public class ClientController {
             if (cpf.length() != 11) System.out.println("CPF deve conter 11 dígitos!");
         } while (cCpf != null || cpf.length() != 11);
         
-        clients.add(new Client(name, cpf));
+        clients.add(new Client(name, cpf)); // adiciona o cliente na lista
         System.out.println("Cliente cadastrado com sucesso!");
         System.out.println("Dados do cliente:");
         clients.get(clients.size() - 1).printInfo();
@@ -44,14 +45,14 @@ public class ClientController {
         sc.nextLine();
     }
 
-    public Client getClientByCpf(String cpf) {
-        return clients.stream().filter(c -> c.getCpf().equals(cpf)).findFirst().orElse(null);
-    }
+    // metodo para buscar cliente por cpf
+    public Client getClientByCpf(String cpf) { return clients.stream().filter(c -> c.getCpf().equals(cpf)).findFirst().orElse(null); }
 
+    // metodo para listar clientes
     public void listClients() {
         Scanner sc = new Scanner(System.in);
-        if(clients.isEmpty()) System.out.println("Nenhum cliente cadastrado!");
-        else clients.forEach(c -> c.printInfo());
+        if(clients.isEmpty()) System.out.println("Nenhum cliente cadastrado!"); // se a lista estiver vazia
+        else clients.forEach(c -> c.printInfo()); // imprime os dados de cada cliente
 
         System.out.println("Pressione ENTER para continuar...");
         sc.nextLine();
